332dd67670d5027597d0eeda37188920a7b33734
Vigorge <alex.pisckunoff@yandex.ru>
2020-12-13 01:17:05 +0300

    Scripted auto-commit on change (2020-12-13 01:17:05) by gitwatch.sh

--- src/main/java/lab4/actors/StorageActor.java
+++ src/main/java/lab4/actors/StorageActor.java
@@ -14,7 +14,7 @@ public class StorageActor extends AbstractActor {
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(TestResult.class, r -> {
-                    storage.put(r.ge)
+                    storage.put(r.getPackID())
                 })
                 .build();
     }
