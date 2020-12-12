d7ec78de3decf86e6ae0b8e092b20a6231af7c7e
Vigorge <alex.pisckunoff@yandex.ru>
2020-12-13 00:21:46 +0300

    Scripted auto-commit on change (2020-12-13 00:21:46) by gitwatch.sh

--- src/main/java/lab4/TaskTestingApp.java
+++ src/main/java/lab4/TaskTestingApp.java
@@ -1,10 +1,12 @@
 package lab4;
 
 import akka.actor.ActorSystem;
+import akka.http.javadsl.Http;
 
 public class TaskTestingApp {
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("webtest");
-        
+        final Http http =
+
     }
 }
