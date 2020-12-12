3c8b2b0f5fd1c3c62e108293fc3fb458cf0c1678
Vigorge <alex.pisckunoff@yandex.ru>
2020-12-13 00:23:06 +0300

    Scripted auto-commit on change (2020-12-13 00:23:06) by gitwatch.sh

--- src/main/java/lab4/TaskTestingApp.java
+++ src/main/java/lab4/TaskTestingApp.java
@@ -8,7 +8,7 @@ public class TaskTestingApp {
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("webtest");
         final Http http = Http.get(system);
-        final ActorMaterializer materializer = 
+        final ActorMaterializer materializer = ActorMaterializer.
 
     }
 }
