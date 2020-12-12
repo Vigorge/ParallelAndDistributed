e6e486b3e46a818c8cb61aa736aaf53f9cbee914
Vigorge <alex.pisckunoff@yandex.ru>
2020-12-13 01:45:55 +0300

    Scripted auto-commit on change (2020-12-13 01:45:55) by gitwatch.sh

--- src/main/java/lab4/actors/StorageActor.java
+++ src/main/java/lab4/actors/StorageActor.java
@@ -4,20 +4,22 @@ import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 import lab4.asists.GetMessage;
 import lab4.asists.PutMessage;
+import lab4.asists.ResultMessage;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class StorageActor extends AbstractActor {
-    private Map<String, ArrayList<String>> storage = new HashMap<>();
+    private Map<String, List<String>> storage = new HashMap<>();
 
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(PutMessage.class, r ->
                         storage.put(r.getPackID(), r.getResult()))
                 .match(GetMessage.class, r ->
-                        sender().tell(new PutMessage(r.getPackID(), storage.get(r.getPackID())), self()))
+                        sender().tell(new ResultMessage(r.getPackID(), storage.get(r.getPackID())), self()))
                 .build();
     }
 }
