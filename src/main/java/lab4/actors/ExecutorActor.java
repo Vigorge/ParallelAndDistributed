f391b886ed8e67551389e02c429bf9cc3076c4c0
Vigorge <alex.pisckunoff@yandex.ru>
2020-12-13 01:26:22 +0300

    Scripted auto-commit on change (2020-12-13 01:26:22) by gitwatch.sh

--- src/main/java/lab4/actors/ExecutorActor.java
+++ src/main/java/lab4/actors/ExecutorActor.java
@@ -1,4 +1,18 @@
 package lab4.actors;
 
-public class ExecutorActor {
+import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
+import lab4.asists.GetMessage;
+import lab4.asists.PutMessage;
+
+public class ExecutorActor extends AbstractActor {
+
+    public Receive createReceive() {
+        return ReceiveBuilder.create()
+                .match(PutMessage.class, r ->
+                        storage.put(r.getPackID(), r.getResult()))
+                .match(GetMessage.class, r ->
+                        sender().tell(new PutMessage(r.getPackID(), storage.get(r.getPackID())), self()))
+                .build();
+    }
 }
