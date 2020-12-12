a19d3bb96d37074b399b0511f46800f757f6fc99
Vigorge <alex.pisckunoff@yandex.ru>
2020-12-13 01:48:37 +0300

    Scripted auto-commit on change (2020-12-13 01:48:37) by gitwatch.sh

--- src/main/java/lab4/actors/StorageActor.java
+++ src/main/java/lab4/actors/StorageActor.java
@@ -6,18 +6,17 @@ import lab4.asists.GetMessage;
 import lab4.asists.PutMessage;
 import lab4.asists.ResultMessage;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class StorageActor extends AbstractActor {
-    private Map<String, List<String>> storage = new HashMap<>();
+    private Map<String, ArrayList<String>> storage = new Hashtable<>();
 
     public Receive createReceive() {
         return ReceiveBuilder.create()
-                .match(PutMessage.class, r ->
-                        storage.put(r.getPackID(), r.getResult()))
+                .match(PutMessage.class, r -> {
+                        ArrayList<String>  
+                        storage.put(r.getPackID(), r.getResult());
+                        })
                 .match(GetMessage.class, r ->
                         sender().tell(new ResultMessage(r.getPackID(), storage.get(r.getPackID())), self()))
                 .build();
