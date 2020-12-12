180131789d4434763dcc5baab8215e6cac41bea1
Vigorge <alex.pisckunoff@yandex.ru>
2020-12-13 01:07:37 +0300

    Scripted auto-commit on change (2020-12-13 01:07:37) by gitwatch.sh

--- src/main/java/lab4/actors/StorageActor.java
+++ src/main/java/lab4/actors/StorageActor.java
@@ -3,11 +3,13 @@ package lab4.actors;
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
+import java.security.MessageDigest;
+
 public class StorageActor extends AbstractActor {
 
     public Receive createReceive() {
         return ReceiveBuilder.create()
-                .match()
+                .match(Message.class)
                 .build();
     }
 }
