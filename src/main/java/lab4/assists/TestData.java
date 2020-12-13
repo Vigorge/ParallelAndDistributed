fcb4c1c8937272d743f96606f1c452edc80d983b
Vigorge <alex.pisckunoff@yandex.ru>
2020-12-13 15:17:58 +0300

    Scripted auto-commit on change (2020-12-13 15:17:58) by gitwatch.sh

--- src/main/java/lab4/assists/TestData.java
+++ src/main/java/lab4/assists/TestData.java
@@ -3,5 +3,6 @@ package lab4.assists;
 import java.io.Serializable;
 
 public class TestData implements Serializable {
-    
+    private static final long serialVersionUID = 1L;
+    private String
 }
