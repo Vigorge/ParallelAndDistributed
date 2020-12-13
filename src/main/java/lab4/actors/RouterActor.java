deb2c36f6b71cc3efaa1b3a4d31985d0ea39c1ce
Vigorge <alex.pisckunoff@yandex.ru>
2020-12-13 17:58:07 +0300

    Scripted auto-commit on change (2020-12-13 17:58:07) by gitwatch.sh

--- src/main/java/lab4/actors/RouterActor.java
+++ src/main/java/lab4/actors/RouterActor.java
@@ -7,7 +7,7 @@ import akka.routing.Router;
 
 public class RouterActor extends AbstractActor {
     private Router router;
-    private ActorRef storage = getContext().actorOf(Props.create())
+    private ActorRef storage = getContext().actorOf(Props.create(S))
 
     @Override
     public Receive createReceive() {
