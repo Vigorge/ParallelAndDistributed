89274c12472bd95f983063da5e2274a09402d8b9
Vigorge <alex.pisckunoff@yandex.ru>
2020-12-13 16:09:21 +0300

    Scripted auto-commit on change (2020-12-13 16:09:21) by gitwatch.sh

--- src/main/java/lab4/messages/ExecMessage.java
+++ src/main/java/lab4/messages/ExecMessage.java
@@ -1,4 +1,21 @@
 package lab4.messages;
 
+import lab4.assists.TestData;
+
 public class ExecMessage {
+    private final String packID;
+    private final TestData testData;
+
+    public ExecMessage(String packID, TestData testData) {
+        this.packID = packID;
+        this.testData = testData;
+    }
+
+    public String getPackID() {
+        return packID;
+    }
+
+    public TestData getTestData() {
+        return testData;
+    }
 }
