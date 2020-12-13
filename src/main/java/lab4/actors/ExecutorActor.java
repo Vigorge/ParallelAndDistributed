83a315522c79764eb427504c61a10260738a1cab
Vigorge <alex.pisckunoff@yandex.ru>
2020-12-13 15:27:24 +0300

    Scripted auto-commit on change (2020-12-13 15:27:24) by gitwatch.sh

--- src/main/java/lab4/actors/ExecutorActor.java
+++ src/main/java/lab4/actors/ExecutorActor.java
@@ -3,11 +3,21 @@ package lab4.actors;
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
 public class ExecutorActor extends AbstractActor {
 
     public Receive createReceive() {
         return ReceiveBuilder.create()
-                .match()
+                .matchAny(r -> {
+                    ScriptEngine engine = new
+                            ScriptEngineManager().getEngineByName("nashorn");
+                    engine.eval(jscript);
+                    Invocable invocable = (Invocable) engine;
+                    return invocable.invokeFunction(functionName, params).toString();
+                })
                 .build();
     }
 }
--- src/main/java/lab4/assists/TestData.java
+++ src/main/java/lab4/assists/TestData.java
@@ -1,9 +1,13 @@
 package lab4.assists;
 
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
 import java.io.Serializable;
 
 public class TestData implements Serializable {
     private static final long serialVersionUID = 1L;
     private final String testName, params;
-    invocable.
+
+
 }
