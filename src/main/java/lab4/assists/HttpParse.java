df94dffa32ba8d6ef545784736a019077f35da63
Vigorge <alex.pisckunoff@yandex.ru>
2020-12-13 19:33:11 +0300

    Scripted auto-commit on change (2020-12-13 19:33:11) by gitwatch.sh

--- src/main/java/lab4/assists/HttpParse.java
+++ src/main/java/lab4/assists/HttpParse.java
@@ -1,4 +1,10 @@
 package lab4.assists;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+
 public class HttpParse {
+    private final ActorRef router;
+
+    public HttpParse(ActorSystem sys)
 }
