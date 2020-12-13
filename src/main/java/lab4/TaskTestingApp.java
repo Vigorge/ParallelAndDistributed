67fcb8950b672b1d5a4a3c53f87bd839f40ce286
Vigorge <alex.pisckunoff@yandex.ru>
2020-12-13 20:23:13 +0300

    Scripted auto-commit on change (2020-12-13 20:23:13) by gitwatch.sh

--- src/main/java/lab4/TaskTestingApp.java
+++ src/main/java/lab4/TaskTestingApp.java
@@ -33,6 +33,6 @@ public class TaskTestingApp {
         );
         System.out.printf("Server online at https://%s:%d/\nPress ENTER to stop\n", HOST, PORT);
         System.in.read();
-        //binding.thenCompose(ServerBinding::unbind).thenAccept(unbound -> system.terminate());
+        binding.thenCompose(ServerBinding::unbind).thenAccept(unbound -> system.terminate());
     }
 }
