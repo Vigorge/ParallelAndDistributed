d46c4039b7c05c601da64ac25326cc462114d579
Vigorge <alex.pisckunoff@yandex.ru>
2020-12-13 21:15:59 +0300

    Scripted auto-commit on change (2020-12-13 21:15:59) by gitwatch.sh

--- src/main/java/lab4/assists/TestData.java
+++ src/main/java/lab4/assists/TestData.java
@@ -6,15 +6,18 @@ import org.apache.htrace.shaded.fasterxml.jackson.annotation.JsonProperty;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public class TestData implements Serializable {
-    private static final long serialVersionUID = 1L;
-    private final String testName, expectedResult;
+public class TestData {
+    @JsonProperty("testName")
+    private final String testName;
+    @JsonProperty("expectedResult")
+    private final String expectedResult;
+    @JsonProperty("params")
     private final ArrayList<Object> params;
 
     @JsonCreator
     public TestData(@JsonProperty("testName") String testName,
                     @JsonProperty("expectedResult") String expectedResult,
-                    @JsonProperty("params")ArrayList<Object> params)
+                    @JsonProperty("params") ArrayList<Object> params)
     {
         this.testName = testName;
         this.expectedResult = expectedResult;
