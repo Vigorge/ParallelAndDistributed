35462631f8e10d08587d6a9e944123bee8de4239
Vigorge <alex.pisckunoff@yandex.ru>
2020-12-13 20:19:12 +0300

    Scripted auto-commit on change (2020-12-13 20:19:12) by gitwatch.sh

--- src/main/java/lab4/TaskTestingApp.java
+++ src/main/java/lab4/TaskTestingApp.java
@@ -24,7 +24,7 @@ public class TaskTestingApp {
         ActorRef routerActor = system.actorOf(Props.create(RouterActor.class), "router");
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
-        HttpParse 
+        HttpParse instanse
         //добавить обработку запросов
         //final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
         //final CompletionStage<ServerBinding> binding = http.bindAndHandle()
