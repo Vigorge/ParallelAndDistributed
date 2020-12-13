df039fb7fe4168ee6fa19523d7bbfe1f0e9fc628
Vigorge <alex.pisckunoff@yandex.ru>
2020-12-13 15:27:53 +0300

    Scripted auto-commit on change (2020-12-13 15:27:53) by gitwatch.sh

--- src/main/java/lab4/actors/ExecutorActor.java
+++ src/main/java/lab4/actors/ExecutorActor.java
@@ -3,21 +3,11 @@ package lab4.actors;
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
-import javax.script.Invocable;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-
 public class ExecutorActor extends AbstractActor {
 
     public Receive createReceive() {
         return ReceiveBuilder.create()
-                .matchAny(r -> {
-                    ScriptEngine engine = new
-                            ScriptEngineManager().getEngineByName("nashorn");
-                    engine.eval(jscript);
-                    Invocable invocable = (Invocable) engine;
-                    return invocable.invokeFunction(functionName, params).toString();
-                })
+                .match()
                 .build();
     }
 }
