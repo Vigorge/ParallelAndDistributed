bc605ae4136378535e467e3b44dfae6069d0d8f7
Vigorge <alex.pisckunoff@yandex.ru>
2020-12-14 19:00:17 +0300

    Scripted auto-commit on change (2020-12-14 19:00:17) by gitwatch.sh

--- src/main/java/lab5/HttpParse.java
+++ src/main/java/lab5/HttpParse.java
@@ -1,4 +1,36 @@
 package lab5;
 
+import akka.actor.ActorRef;
+import akka.http.javadsl.marshallers.jackson.Jackson;
+import akka.http.javadsl.server.Route;
+import akka.pattern.Patterns;
+import akka.util.Timeout;
+import lab4.assists.PackageData;
+import lab4.messages.GetMessage;
+import scala.concurrent.Future;
+
+import java.time.Duration;
+
+import static akka.http.javadsl.server.Directives.*;
+
 public class HttpParse {
+    private final ActorRef router;
+    private final static Timeout TIMEOUT = Timeout.create(Duration.ofSeconds(5));
+
+    public HttpParse(ActorRef router) {
+        this.router = router;
+    }
+
+    public Route createRoute() {
+        return route(
+                get(() -> parameter("packageID", (pID) -> {
+                    Future<Object> future = Patterns.ask(router, new GetMessage(pID), TIMEOUT);
+                    return completeOKWithFuture(future, Jackson.marshaller());
+                })),
+                post(() -> entity(Jackson.unmarshaller(PackageData.class), msg -> {
+                    router.tell(msg, ActorRef.noSender());
+                    return complete("Tests executed and stored");
+                }))
+        );
+    }
 }
