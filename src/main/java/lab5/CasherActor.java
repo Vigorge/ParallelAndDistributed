644e3916575ced5bed8d23ca268e40414b7d1473
Vigorge <alex.pisckunoff@yandex.ru>
2020-12-14 20:06:43 +0300

    Scripted auto-commit on change (2020-12-14 20:06:43) by gitwatch.sh

--- src/main/java/lab5/CasherActor.java
+++ src/main/java/lab5/CasherActor.java
@@ -2,12 +2,15 @@ package lab5;
 
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
 import akka.japi.pf.ReceiveBuilder;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class CasherActor extends AbstractActor {
+    private final LoggingAdapter log = Logging.getLogger(getContext().getSystem(), self());
     private final Map<String, Float> cash = new HashMap<>();
 
     public Receive createReceive() {
@@ -16,7 +19,7 @@ public class CasherActor extends AbstractActor {
                         sender().tell(cash.get(r), ActorRef.noSender()))
                 .match(StoreMessage.class, r ->
                         cash.put(r.getUrl(), r.getAvgTime()))
-                .matchAny()
+                .matchAny(o -> log.info("recieved unknown message"))
                 .build();
     }
 }
