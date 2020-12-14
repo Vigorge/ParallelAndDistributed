054b07d27b0118a8b6c119d785de8cf0bd8038a0
Vigorge <alex.pisckunoff@yandex.ru>
2020-12-14 20:04:51 +0300

    Scripted auto-commit on change (2020-12-14 20:04:51) by gitwatch.sh

--- src/main/java/lab5/StoreMessage.java
+++ src/main/java/lab5/StoreMessage.java
@@ -1,5 +1,11 @@
 package lab5;
 
 public class StoreMessage {
-    private final 
+    private final String url;
+    private final float avgTime;
+
+    public StoreMessage(String url, float avgTime) {
+        this.url = url;
+        this.avgTime = avgTime;
+    }
 }
