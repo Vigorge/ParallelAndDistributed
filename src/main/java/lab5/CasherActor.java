4b03c5b06428eb9c577034c805239681384e98db
Vigorge <alex.pisckunoff@yandex.ru>
2020-12-14 19:46:59 +0300

    Scripted auto-commit on change (2020-12-14 19:46:59) by gitwatch.sh

--- src/main/java/lab5/CasherActor.java
+++ src/main/java/lab5/CasherActor.java
@@ -1,11 +1,13 @@
 package lab5;
 
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
 
 public class CasherActor extends AbstractActor {
 
 
     public Receive createReceive() {
-        return ;
+        return ReceiveBuilder.create()
+                .match();
     }
 }
