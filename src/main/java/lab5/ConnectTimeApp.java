b81a3911405d36878bb36c68d09feab4d57c0179
Vigorge <alex.pisckunoff@yandex.ru>
2020-12-14 18:59:26 +0300

    Scripted auto-commit on change (2020-12-14 18:59:26) by gitwatch.sh

--- src/main/java/lab5/ConnectTimeApp.java
+++ src/main/java/lab5/ConnectTimeApp.java
@@ -1,4 +1,38 @@
 package lab5;
 
+import akka.NotUsed;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+import lab4.actors.RouterActor;
+import lab4.assists.HttpParse;
+
+import java.util.concurrent.CompletionStage;
+
 public class ConnectTimeApp {
+    private static final int PORT = 8088;
+    private static final String HOST = "localhost";
+    public static void main(String[] args) throws Exception {
+        ActorSystem system = ActorSystem.create("webtest");
+        ActorRef routerActor = system.actorOf(Props.create(RouterActor.class), "router");
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        HttpParse instance = new HttpParse(routerActor);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = instance.createRoute().flow(system, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost(HOST, PORT),
+                materializer
+        );
+        System.out.printf("Server online at https://%s:%d/\n", HOST, PORT);
+        System.in.read();
+        binding.thenCompose(ServerBinding::unbind).thenAccept(unbound -> system.terminate());
+    }
 }
